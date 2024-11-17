d48c810d9dbc2ceaeaae692b98e4ebea1652205e
ntannn <nhattanff07@gmail.com>
2024-11-17 16:05:22 +0700

    Update personal information in AboutCard component

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Đào Nhật Tân </span>
+            Hi Everyone, I am <strong><span className="purple">Đào Nhật Tân </span></strong>
             from <span className="purple"> Ha Tinh, Viet Nam.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am student in <strong><span className="purple">Ha Tinh high school for Gifted student (CHT)</span></strong>.
             <br />
             I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
             Mesra.
