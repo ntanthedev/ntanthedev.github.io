699c88e70d6d4011bdcca77bd789498dcd239023
ntannn <nhattanff07@gmail.com>
2024-11-17 16:01:58 +0700

    Update location in AboutCard

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Đào Nhật Tân </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Ha Tinh, Viet Nam.</span>
             <br />
             I am currently employed as a software developer at Juspay.
             <br />
