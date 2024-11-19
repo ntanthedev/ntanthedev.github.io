9ae93413594b28adb020734ee15f25cdc2a113fa
ntannn <nhattanff07@gmail.com>
2024-11-19 20:13:03 +0700

    Update Navbar link, modify portfolio title and meta tags, and refresh asset manifest for new build files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/ntanthedev"
+                href="https://github.com/ntanthedev/ntanthedev.github.io"
                 target="_blank"
                 className="fork-btn-inner"
               >
