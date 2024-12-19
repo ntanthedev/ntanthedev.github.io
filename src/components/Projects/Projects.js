f21e5d5eeb8148efb8ce4bdc058a17b5ce622ab8
ntan <nhattanff07@gmail.com>
2024-12-19 09:07:56 +0700

    Update project card to showcase URL Shortener and refresh asset manifest

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import shorterurl from "../../Assets/Projects/shorterurl.png";
 import chtoj from "../../Assets/Projects/chtoj.png";
 import neoteam from "../../Assets/Projects/neoteam.png";
 import chtcoder from "../../Assets/Projects/chtcoder.png";
@@ -56,12 +56,12 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={shorterurl}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="URL-Shorten-Worker"
+              description="A URL shortener use for Cloudflare Workers using javascript. This is a serverless function that takes a long URL and returns a short URL. The short URL redirects to the original long URL. I customize it from crazypeace."
+              ghLink="https://github.com/ntanthedev/Url-Shorten-Worker"
+              demoLink="https://ntannn.online/shorturl/"
             />
           </Col>
 
